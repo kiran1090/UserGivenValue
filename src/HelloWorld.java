fce7afe4cb5aa97f976bd1abf6b51f4434013dd2
kiran_a <iamkiranashok@gmail.com>
2022-12-19 15:35:33 +0530

    second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class HelloWorld {
+import java.util.Scanner;
+
+class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        // nextInt() reads the next integer from the keyboard
+        int num = reader.nextInt();
+
+        // println() prints the following line to the output screen
+        System.out.println("The number entered is " + num);
+    }
 }
